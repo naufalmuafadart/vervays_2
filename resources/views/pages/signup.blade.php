ffef22f2426f63397263fe319cc0a8ebeca8153d
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2022-02-15 10:57:02 +0700

    Menambahkan favicon

--- resources/views/pages/signup.blade.php
+++ resources/views/pages/signup.blade.php
@@ -9,6 +9,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+  <link rel="icon" type="image/x-icon" href="/image/favicon.ico">
   <link rel="stylesheet" href="/css/app.css">
 
   <link rel="stylesheet" href="/css/signup.css">
