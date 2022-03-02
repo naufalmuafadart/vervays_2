9243e16a5b295d6429e2a066e2eb43cf694c0dbe
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2022-03-03 05:05:43 +0700

    Membuat halaman error 404

---
+++ resources/views/pages/error/404.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>404 - Tidak Ditemukan</title>
+  @include('includes.style')
+  <link rel="stylesheet" href="/css/error/404.css">
+</head>
+<body>
+  <h1>404</h1>
+  <p>Halaman atau token tidak ditemukan atau tidak tersedia</p>
+  <a href="/">Kembali ke halaman utama</a>
+</body>
+</html>
