87bf06003e71ff848240e62d43936334e57ce7e5
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2022-03-02 13:16:05 +0700

    Membuat tampilan home

--- resources/views/pages/buyer/home.blade.php
+++ resources/views/pages/buyer/home.blade.php
@@ -9,7 +9,198 @@
 @endpush
 
 @section('content')
-  <p style="float: right;">TFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF</p>
+  <h1>Buku Terbaru</h1>
+  <div class="grid-container">
+    <a href="#" class="grid-item">
+      <img class="placeholder" src="/image/placeholder/book_placeholder.png" alt="" srcset="">
+      <p class="title">90 Masakan Rumahan Untuk Sebulan-Endang Indriani</p>
+      <p class="author">Endang Indriani</p>
+      <div class="star-container">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star1">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star2">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star3">
+        <img src="/image/icon/blank_star.png" alt="" class="star star4">
+        <img src="/image/icon/blank_star.png" alt="" class="star star5">
+      </div>
+      <p class="prevPrice">Rp. 54.000</p>
+      <p class="currPrice">Rp. 45.000</p>
+    </a>
+    <a href="#" class="grid-item">
+      <img class="placeholder" src="/image/placeholder/book_placeholder.png" alt="" srcset="">
+      <p class="title">90 Masakan Rumahan Untuk Sebulan-Endang Indriani</p>
+      <p class="author">Endang Indriani</p>
+      <div class="star-container">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star1">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star2">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star3">
+        <img src="/image/icon/blank_star.png" alt="" class="star star4">
+        <img src="/image/icon/blank_star.png" alt="" class="star star5">
+      </div>
+      <p class="prevPrice">Rp. 54.000</p>
+      <p class="currPrice">Rp. 45.000</p>
+    </a>
+    <a href="#" class="grid-item">
+      <img class="placeholder" src="/image/placeholder/book_placeholder.png" alt="" srcset="">
+      <p class="title">90 Masakan Rumahan Untuk Sebulan-Endang Indriani</p>
+      <p class="author">Endang Indriani</p>
+      <div class="star-container">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star1">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star2">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star3">
+        <img src="/image/icon/blank_star.png" alt="" class="star star4">
+        <img src="/image/icon/blank_star.png" alt="" class="star star5">
+      </div>
+      <p class="prevPrice">Rp. 54.000</p>
+      <p class="currPrice">Rp. 45.000</p>
+    </a>
+    <a href="#" class="grid-item">
+      <img class="placeholder" src="/image/placeholder/book_placeholder.png" alt="" srcset="">
+      <p class="title">90 Masakan Rumahan Untuk Sebulan-Endang Indriani</p>
+      <p class="author">Endang Indriani</p>
+      <div class="star-container">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star1">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star2">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star3">
+        <img src="/image/icon/blank_star.png" alt="" class="star star4">
+        <img src="/image/icon/blank_star.png" alt="" class="star star5">
+      </div>
+      <p class="prevPrice">Rp. 54.000</p>
+      <p class="currPrice">Rp. 45.000</p>
+    </a>
+    <a href="#" class="grid-item">
+      <img class="placeholder" src="/image/placeholder/book_placeholder.png" alt="" srcset="">
+      <p class="title">90 Masakan Rumahan Untuk Sebulan-Endang Indriani</p>
+      <p class="author">Endang Indriani</p>
+      <div class="star-container">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star1">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star2">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star3">
+        <img src="/image/icon/blank_star.png" alt="" class="star star4">
+        <img src="/image/icon/blank_star.png" alt="" class="star star5">
+      </div>
+      <p class="prevPrice">Rp. 54.000</p>
+      <p class="currPrice">Rp. 45.000</p>
+    </a>
+    <a href="#" class="grid-item">
+      <img class="placeholder" src="/image/placeholder/book_placeholder.png" alt="" srcset="">
+      <p class="title">90 Masakan Rumahan Untuk Sebulan-Endang Indriani</p>
+      <p class="author">Endang Indriani</p>
+      <div class="star-container">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star1">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star2">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star3">
+        <img src="/image/icon/blank_star.png" alt="" class="star star4">
+        <img src="/image/icon/blank_star.png" alt="" class="star star5">
+      </div>
+      <p class="prevPrice">Rp. 54.000</p>
+      <p class="currPrice">Rp. 45.000</p>
+    </a>
+  </div>
+
+  <h1>Pilihan Editor</h1>
+  <div class="grid-container">
+    <a href="#" class="grid-item">
+      <img class="placeholder" src="/image/placeholder/book_placeholder.png" alt="" srcset="">
+      <p class="title">90 Masakan Rumahan Untuk Sebulan-Endang Indriani</p>
+      <p class="author">Endang Indriani</p>
+      <div class="star-container">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star1">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star2">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star3">
+        <img src="/image/icon/blank_star.png" alt="" class="star star4">
+        <img src="/image/icon/blank_star.png" alt="" class="star star5">
+      </div>
+      <p class="prevPrice">Rp. 54.000</p>
+      <p class="currPrice">Rp. 45.000</p>
+    </a>
+    <a href="#" class="grid-item">
+      <img class="placeholder" src="/image/placeholder/book_placeholder.png" alt="" srcset="">
+      <p class="title">90 Masakan Rumahan Untuk Sebulan-Endang Indriani</p>
+      <p class="author">Endang Indriani</p>
+      <div class="star-container">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star1">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star2">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star3">
+        <img src="/image/icon/blank_star.png" alt="" class="star star4">
+        <img src="/image/icon/blank_star.png" alt="" class="star star5">
+      </div>
+      <p class="prevPrice">Rp. 54.000</p>
+      <p class="currPrice">Rp. 45.000</p>
+    </a>
+    <a href="#" class="grid-item">
+      <img class="placeholder" src="/image/placeholder/book_placeholder.png" alt="" srcset="">
+      <p class="title">90 Masakan Rumahan Untuk Sebulan-Endang Indriani</p>
+      <p class="author">Endang Indriani</p>
+      <div class="star-container">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star1">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star2">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star3">
+        <img src="/image/icon/blank_star.png" alt="" class="star star4">
+        <img src="/image/icon/blank_star.png" alt="" class="star star5">
+      </div>
+      <p class="prevPrice">Rp. 54.000</p>
+      <p class="currPrice">Rp. 45.000</p>
+    </a>
+    <a href="#" class="grid-item">
+      <img class="placeholder" src="/image/placeholder/book_placeholder.png" alt="" srcset="">
+      <p class="title">90 Masakan Rumahan Untuk Sebulan-Endang Indriani</p>
+      <p class="author">Endang Indriani</p>
+      <div class="star-container">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star1">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star2">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star3">
+        <img src="/image/icon/blank_star.png" alt="" class="star star4">
+        <img src="/image/icon/blank_star.png" alt="" class="star star5">
+      </div>
+      <p class="prevPrice">Rp. 54.000</p>
+      <p class="currPrice">Rp. 45.000</p>
+    </a>
+    <a href="#" class="grid-item">
+      <img class="placeholder" src="/image/placeholder/book_placeholder.png" alt="" srcset="">
+      <p class="title">90 Masakan Rumahan Untuk Sebulan-Endang Indriani</p>
+      <p class="author">Endang Indriani</p>
+      <div class="star-container">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star1">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star2">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star3">
+        <img src="/image/icon/blank_star.png" alt="" class="star star4">
+        <img src="/image/icon/blank_star.png" alt="" class="star star5">
+      </div>
+      <p class="prevPrice">Rp. 54.000</p>
+      <p class="currPrice">Rp. 45.000</p>
+    </a>
+    <a href="#" class="grid-item">
+      <img class="placeholder" src="/image/placeholder/book_placeholder.png" alt="" srcset="">
+      <p class="title">90 Masakan Rumahan Untuk Sebulan-Endang Indriani</p>
+      <p class="author">Endang Indriani</p>
+      <div class="star-container">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star1">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star2">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star3">
+        <img src="/image/icon/blank_star.png" alt="" class="star star4">
+        <img src="/image/icon/blank_star.png" alt="" class="star star5">
+      </div>
+      <p class="prevPrice">Rp. 54.000</p>
+      <p class="currPrice">Rp. 45.000</p>
+    </a>
+  </div>
+
+  <template>
+    <a href="#" class="grid-item">
+      <img class="placeholder" src="/image/placeholder/book_placeholder.png" alt="" srcset="">
+      <p class="title">90 Masakan Rumahan Untuk Sebulan-Endang Indriani</p>
+      <p class="author">Endang Indriani</p>
+      <div class="star-container">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star1">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star2">
+        <img src="/image/icon/yellow_star.png" alt="" class="star star3">
+        <img src="/image/icon/blank_star.png" alt="" class="star star4">
+        <img src="/image/icon/blank_star.png" alt="" class="star star5">
+      </div>
+      <p class="prevPrice">Rp. 54.000</p>
+      <p class="currPrice">Rp. 45.000</p>
+    </a>
+  </template>
 @endsection
 
 @push('add-on-script')
